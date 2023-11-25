875a410fd0d38c309829db7673642eedc61adadf
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-11-25 20:45:45 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(2, "Electronics");
+        System.out.println(obj);
+    }
+}
