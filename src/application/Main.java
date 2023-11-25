3051763ae1a809971b639896665a9448dbf3b7af
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-11-25 20:49:37 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(2, "Electronics");
+
+        Seller seller = new Seller(23, "Alex", "alex@gmail.com", new Date(), 3000.0, obj);
+
         System.out.println(obj);
     }
 }
