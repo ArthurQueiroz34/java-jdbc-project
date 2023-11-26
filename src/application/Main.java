9d2a39eb1ec04de41146feb0a0ca7345e9052fc1
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-11-25 21:00:36 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,12 +9,12 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(2, "Electronics");
-
-        Seller seller = new Seller(23, "Alex", "alex@gmail.com", new Date(), 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
+
     }
 }
